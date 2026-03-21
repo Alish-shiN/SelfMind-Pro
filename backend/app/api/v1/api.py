e67fe1aa608db1eb6044236e77bf8935c5a57387
Alish-shiN <alisherkulataev860@gmail.com>
2026-03-21 16:33:24 +0500

    feat: add analytic system

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, users, journal
+from app.api.v1.endpoints import auth, users, journal, analytics
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
 api_router.include_router(journal.router)
+api_router.include_router(analytics.router)
